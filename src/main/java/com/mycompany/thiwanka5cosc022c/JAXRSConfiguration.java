5ddd845d42dbd002948091332232c702e135c4bc
Thiwanka1012 <160256984+Thiwanka1012@users.noreply.github.com>
2025-04-20 12:12:13 +0530

    Initial commit

---
+++ src/main/java/com/mycompany/thiwanka5cosc022c/JAXRSConfiguration.java
@@ -0,0 +1,13 @@
+package com.mycompany.thiwanka5cosc022c;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ * Configures JAX-RS for the application.
+ * @author Juneau
+ */
+@ApplicationPath("resources")
+public class JAXRSConfiguration extends Application {
+    
+}
