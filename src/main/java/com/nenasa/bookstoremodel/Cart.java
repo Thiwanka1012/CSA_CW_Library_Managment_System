23216a6a667018cb311be5ada0077c66534a725f
Thiwanka1012 <160256984+Thiwanka1012@users.noreply.github.com>
2025-04-20 17:10:21 +0530

    Add Cart class with customerId and item management methods

--- src/main/java/com/nenasa/bookstoremodel/Cart.java
+++ src/main/java/com/nenasa/bookstoremodel/Cart.java
@@ -15,6 +15,37 @@ public class Cart {
     
     private int customerId;
     private Map<Integer,Integer> item;
+
+    public Cart() {
+    }
+
+    public Cart(int customerId, Map<Integer, Integer> item) {
+        this.customerId = customerId;
+        this.item = item;
+    }
+
+    public int getCustomerId() {
+        return customerId;
+    }
+
+    public Map<Integer, Integer> getItem() {
+        return item;
+    }
+
+    public void setCustomerId(int customerId) {
+        this.customerId = customerId;
+    }
+
+    public void setItem(Map<Integer, Integer> item) {
+        this.item = item;
+    }
+
+    @Override
+    public String toString() {
+        return "Cart{" + "customerId=" + customerId + ", item=" + item + '}';
+    }
+    
+    
     
     
     
