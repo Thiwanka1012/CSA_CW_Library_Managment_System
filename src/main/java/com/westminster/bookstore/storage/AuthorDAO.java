e451857fbf8994a1df719e7f6e669fdee789f22b
Thiwanka1012 <160256984+Thiwanka1012@users.noreply.github.com>
2025-04-24 21:27:44 +0530

    Refactor AuthorDAO class to improve code clarity by removing unused methods and comments; update method parameters for consistency

--- src/main/java/com/westminster/bookstore/storage/AuthorDAO.java
+++ src/main/java/com/westminster/bookstore/storage/AuthorDAO.java
@@ -7,35 +7,28 @@ import java.util.List;
 public class AuthorDAO {
     private static List<Author> authors = new ArrayList<>();
 
-    // Initialize with some sample data
     static {
-        authors.add(new Author(1, "J.R.R. Tolkien", "John Ronald Reuel Tolkien was an English writer, poet, philologist, and academic, best known for The Lord of the Rings."));
-        authors.add(new Author(2, "J.K. Rowling", "Joanne Rowling, known by her pen name J.K. Rowling, is a British author, best known for the Harry Potter series."));
+        authors.add(new Author(1, "George", "Orwell", "Author of 1984 and Animal Farm"));
+        authors.add(new Author(2, "J.K.", "Rowling", "Author of Harry Potter series"));
     }
 
-    // Get all authors
     public List<Author> getAllAuthors() {
-        return new ArrayList<>(authors); // Return a copy to prevent direct modification
+        return new ArrayList<>(authors);
     }
 
-    // Get author by ID
-    public Author getAuthorById(int authorId) {
+    public Author getAuthorById(int id) {
         for (Author author : authors) {
-            if (author.getAuthorId() == authorId) {
+            if (author.getAuthorId() == id) {
                 return author;
             }
         }
         return null;
     }
 
-    // Add a new author
     public void addAuthor(Author author) {
-        int newId = getNextAuthorId();
-        author.setAuthorId(newId);
         authors.add(author);
     }
 
-    // Update an existing author
     public void updateAuthor(Author updatedAuthor) {
         for (int i = 0; i < authors.size(); i++) {
             if (authors.get(i).getAuthorId() == updatedAuthor.getAuthorId()) {
@@ -45,19 +38,7 @@ public class AuthorDAO {
         }
     }
 
-    // Delete an author by ID
-    public void deleteAuthor(int authorId) {
-        authors.removeIf(author -> author.getAuthorId() == authorId);
-    }
-
-    // Generate the next author ID
-    private int getNextAuthorId() {
-        int maxId = 0;
-        for (Author author : authors) {
-            if (author.getAuthorId() > maxId) {
-                maxId = author.getAuthorId();
-            }
-        }
-        return maxId + 1;
+    public void deleteAuthor(int id) {
+        authors.removeIf(author -> author.getAuthorId() == id);
     }
 }
