0c51ae231d279cabcd74e84ea9732770e20147e4
Thiwanka1012 <160256984+Thiwanka1012@users.noreply.github.com>
2025-04-24 22:08:27 +0530

    Implement OutOfStockExceptionMapper to handle out-of-stock exceptions and return appropriate error responses

--- src/main/java/com/westminster/bookstore/exceptions/OutOfStockExceptionMapper.java
+++ src/main/java/com/westminster/bookstore/exceptions/OutOfStockExceptionMapper.java
@@ -1,13 +1,18 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.westminster.bookstore.exceptions;
 
-/**
- *
- * @author Thiwanka-Gaming
- */
-public class OutOfStockExceptionMapper {
-    
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.ExceptionMapper;
+import javax.ws.rs.ext.Provider;
+import java.util.HashMap;
+import java.util.Map;
+
+@Provider
+public class OutOfStockExceptionMapper implements ExceptionMapper<OutOfStockException> {
+    @Override
+    public Response toResponse(OutOfStockException exception) {
+        Map<String, String> error = new HashMap<>();
+        error.put("error", "Out of Stock");
+        error.put("message", exception.getMessage());
+        return Response.status(Response.Status.BAD_REQUEST).entity(error).build();
+    }
 }
