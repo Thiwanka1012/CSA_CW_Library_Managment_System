b27f0bebd88bd11f2ee95be4ce51d0eef1bb9c67
Thiwanka1012 <160256984+Thiwanka1012@users.noreply.github.com>
2025-04-24 21:34:39 +0530

    Refactor BookDAO class to remove unused methods and improve code clarity; update book retrieval and deletion methods for consistency.

--- src/main/java/com/westminster/bookstore/dao/BookDAO.java
+++ src/main/java/com/westminster/bookstore/dao/BookDAO.java
@@ -7,35 +7,28 @@ import java.util.List;
 public class BookDAO {
     private static List<Book> books = new ArrayList<>();
 
-    // Initialize with some sample data
     static {
-        books.add(new Book("The Lord of the Rings", 1, "J.R.R. Tolkien", "9780618053267", 1954, 20.99, 100));
-        books.add(new Book("Harry Potter and the Philosopher's Stone", 2, "J.K. Rowling", "9780747532699", 1997, 15.99, 50));
+        books.add(new Book("1984", 1, 1, "978-0-452-28423-4", 1949, 9.99, 100));
+        books.add(new Book("Harry Potter and the Philosopher's Stone", 2, 2, "978-0-7475-3269-9", 1997, 12.99, 50));
     }
 
-    // Get all books
     public List<Book> getAllBooks() {
-        return new ArrayList<>(books); // Return a copy to prevent direct modification
+        return new ArrayList<>(books);
     }
 
-    // Get book by ID
-    public Book getBookById(int bookId) {
+    public Book getBookById(int id) {
         for (Book book : books) {
-            if (book.getBookId() == bookId) {
+            if (book.getBookId() == id) {
                 return book;
             }
         }
         return null;
     }
 
-    // Add a new book
     public void addBook(Book book) {
-        int newId = getNextBookId();
-        book.setBookId(newId);
         books.add(book);
     }
 
-    // Update an existing book
     public void updateBook(Book updatedBook) {
         for (int i = 0; i < books.size(); i++) {
             if (books.get(i).getBookId() == updatedBook.getBookId()) {
@@ -45,19 +38,7 @@ public class BookDAO {
         }
     }
 
-    // Delete a book by ID
-    public void deleteBook(int bookId) {
-        books.removeIf(book -> book.getBookId() == bookId);
-    }
-
-    // Generate the next book ID
-    private int getNextBookId() {
-        int maxId = 0;
-        for (Book book : books) {
-            if (book.getBookId() > maxId) {
-                maxId = book.getBookId();
-            }
-        }
-        return maxId + 1;
+    public void deleteBook(int id) {
+        books.removeIf(book -> book.getBookId() == id);
     }
 }
