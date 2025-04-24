f2cb47f38a6e2792db647198436dbcd1a21a0efe
Thiwanka1012 <160256984+Thiwanka1012@users.noreply.github.com>
2025-04-24 15:45:16 +0530

    Implement BookNotFoundExceptionMapper to handle BookNotFoundException and return appropriate error response

--- src/main/java/com/westminster/bookstore/exceptions/BookNotFoundExceptionMapper.java
+++ src/main/java/com/westminster/bookstore/exceptions/BookNotFoundExceptionMapper.java
@@ -1,13 +1,18 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.westminster.bookstore.exceptions;
 
-/**
- *
- * @author Thiwanka-Gaming
- */
-public class BookNotFoundExceptionMapper {
-    
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.ExceptionMapper;
+import javax.ws.rs.ext.Provider;
+import java.util.HashMap;
+import java.util.Map;
+
+@Provider
+public class BookNotFoundExceptionMapper implements ExceptionMapper<BookNotFoundException> {
+    @Override
+    public Response toResponse(BookNotFoundException exception) {
+        Map<String, String> error = new HashMap<>();
+        error.put("error", "Book Not Found");
+        error.put("message", exception.getMessage());
+        return Response.status(Response.Status.NOT_FOUND).entity(error).build();
+    }
 }
