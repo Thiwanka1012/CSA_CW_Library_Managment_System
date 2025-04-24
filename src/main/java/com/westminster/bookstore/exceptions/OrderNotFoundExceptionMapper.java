6ab28ed7c98fd51bcac76ed39900e9ca1b7ba058
Thiwanka1012 <160256984+Thiwanka1012@users.noreply.github.com>
2025-04-24 16:04:45 +0530

    Implement OrderNotFoundExceptionMapper to handle OrderNotFoundException and return appropriate error response

--- src/main/java/com/westminster/bookstore/exceptions/OrderNotFoundExceptionMapper.java
+++ src/main/java/com/westminster/bookstore/exceptions/OrderNotFoundExceptionMapper.java
@@ -1,13 +1,18 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.westminster.bookstore.exceptions;
 
-/**
- *
- * @author Thiwanka-Gaming
- */
-public class OrderNotFoundExceptionMapper {
-    
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.ExceptionMapper;
+import javax.ws.rs.ext.Provider;
+import java.util.HashMap;
+import java.util.Map;
+
+@Provider
+public class OrderNotFoundExceptionMapper implements ExceptionMapper<OrderNotFoundException> {
+    @Override
+    public Response toResponse(OrderNotFoundException exception) {
+        Map<String, String> error = new HashMap<>();
+        error.put("error", "Order Not Found");
+        error.put("message", exception.getMessage());
+        return Response.status(Response.Status.NOT_FOUND).entity(error).build();
+    }
 }
