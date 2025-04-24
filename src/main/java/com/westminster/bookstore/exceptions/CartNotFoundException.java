1ed19b31c683fa26aed9badf83fb2b2899ec1370
Thiwanka1012 <160256984+Thiwanka1012@users.noreply.github.com>
2025-04-24 16:13:11 +0530

    Implement CartNotFoundException class extending RuntimeException for handling cart not found errors

--- src/main/java/com/westminster/bookstore/exceptions/CartNotFoundException.java
+++ src/main/java/com/westminster/bookstore/exceptions/CartNotFoundException.java
@@ -1,13 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.westminster.bookstore.exceptions;
 
-/**
- *
- * @author Thiwanka-Gaming
- */
-public class CartNotFoundException {
-    
+public class CartNotFoundException extends RuntimeException {
+    public CartNotFoundException(String message) {
+        super(message);
+    }
 }
