1f614a789c446219f3f68cfa7fde413cbffad30c
Thiwanka1012 <160256984+Thiwanka1012@users.noreply.github.com>
2025-04-24 16:21:08 +0530

    Update InvalidInputException to extend RuntimeException and add constructor; update maven-war-plugin version to 3.4.0

--- src/main/java/com/westminster/bookstore/exceptions/InvalidInputException.java
+++ src/main/java/com/westminster/bookstore/exceptions/InvalidInputException.java
@@ -1,13 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.westminster.bookstore.exceptions;
 
-/**
- *
- * @author Thiwanka-Gaming
- */
-public class InvalidInputException {
-    
+public class InvalidInputException extends RuntimeException {
+    public InvalidInputException(String message) {
+        super(message);
+    }
 }
