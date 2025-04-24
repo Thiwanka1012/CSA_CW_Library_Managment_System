cc84e1663f2d6dc936ac5a04aafe3642bfb127c1
Thiwanka1012 <160256984+Thiwanka1012@users.noreply.github.com>
2025-04-24 21:34:12 +0530

    Refactor Book class to replace authorName with authorId; update constructor and getter/setter methods accordingly; improve toString method for clarity.

--- src/main/java/com/westminster/bookstore/model/Book.java
+++ src/main/java/com/westminster/bookstore/model/Book.java
@@ -1,95 +1,77 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.westminster.bookstore.model;
 
-/**
- *
- * @author Thiwanka-Gaming
- */
-
-//Attributes of Book
 public class Book {
-    
-    private String title;
     private int bookId;
-    private String authorName;
+    private String title;
+    private int authorId;
     private String isbn;
     private int publicationYear;
     private double price;
     private int stockQuantity;
 
-    
-    //Non parameterized Constructor
     public Book() {
     }
-    
 
-    //Parameterized Constructor
-    public Book(String title, int bookId, String authorName, String isbn, int publicationYear, double price, int stockQuantity) {
+    public Book(String title, int bookId, int authorId, String isbn, int publicationYear, double price, int stockQuantity) {
         this.title = title;
         this.bookId = bookId;
-        this.authorName = authorName;
+        this.authorId = authorId;
         this.isbn = isbn;
         this.publicationYear = publicationYear;
         this.price = price;
         this.stockQuantity = stockQuantity;
     }
 
-    //Getter Methods
-    public String getTitle() {
-        return title;
-    }
-
     public int getBookId() {
         return bookId;
-    }
-
-    public String getAuthorName() {
-        return authorName;
-    }
-
-    public String getIsbn() {
-        return isbn;
-    }
-
-    public int getPublicationYear() {
-        return publicationYear;
-    }
-
-    public double getPrice() {
-        return price;
-    }
-
-    public int getStockQuantity() {
-        return stockQuantity;
-    }
-
-    
-    //Setter Methods
-    public void setTitle(String title) {
-        this.title = title;
     }
 
     public void setBookId(int bookId) {
         this.bookId = bookId;
     }
 
-    public void setAuthorName(String authorName) {
-        this.authorName = authorName;
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getAuthorId() {
+        return authorId;
+    }
+
+    public void setAuthorId(int authorId) {
+        this.authorId = authorId;
+    }
+
+    public String getIsbn() {
+        return isbn;
     }
 
     public void setIsbn(String isbn) {
         this.isbn = isbn;
     }
 
+    public int getPublicationYear() {
+        return publicationYear;
+    }
+
     public void setPublicationYear(int publicationYear) {
         this.publicationYear = publicationYear;
     }
 
+    public double getPrice() {
+        return price;
+    }
+
     public void setPrice(double price) {
         this.price = price;
+    }
+
+    public int getStockQuantity() {
+        return stockQuantity;
     }
 
     public void setStockQuantity(int stockQuantity) {
@@ -98,14 +80,6 @@ public class Book {
 
     @Override
     public String toString() {
-        return "Book{" + "title=" + title + ", bookId=" + bookId + ", authorName=" + authorName + ", isbn=" + isbn + ", publicationYear=" + publicationYear + ", price=" + price + ", stockQuantity=" + stockQuantity + '}';
+        return "Book{" + "bookId=" + bookId + ", title=" + title + ", authorId=" + authorId + ", isbn=" + isbn + ", publicationYear=" + publicationYear + ", price=" + price + ", stockQuantity=" + stockQuantity + '}';
     }
-    
-    
-    
-    
-    
 }
-
-
-
