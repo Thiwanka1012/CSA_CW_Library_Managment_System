b9c500f3ab7b3202212ec076df3d6dbfe5cace27
Thiwanka1012 <160256984+Thiwanka1012@users.noreply.github.com>
2025-04-25 18:40:03 +0530

    Refactor ApplicationConfig to extend ResourceConfig and register necessary features for JSON handling and resource mapping

--- src/main/java/com/westminster/bookstore/config/ApplicationConfig.java
+++ src/main/java/com/westminster/bookstore/config/ApplicationConfig.java
@@ -1,13 +1,18 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package com.westminster.bookstore;
+package com.westminster.bookstore.config;
 
-/**
- *
- * @author Thiwanka-Gaming
- */
-public class ApplicationConfig {
-    
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.glassfish.jersey.jackson.JacksonFeature;
+import org.glassfish.jersey.server.ResourceConfig;
+
+public class ApplicationConfig extends ResourceConfig {
+    public ApplicationConfig() {
+        // Register resource and exception mapper packages
+        packages("com.westminster.bookstore.resources", "com.westminster.bookstore.exceptions");
+
+        // Register JacksonFeature for JSON serialization/deserialization
+        register(JacksonFeature.class);
+
+        // Register JavaTimeModule to handle Java 8 date/time types
+        register(new JavaTimeModule());
+    }
 }
