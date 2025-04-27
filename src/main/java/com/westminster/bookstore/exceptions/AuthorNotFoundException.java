e802778d3430ca1dc39793fe218ca7b18175626d
Thiwanka1012 <160256984+Thiwanka1012@users.noreply.github.com>
2025-04-28 02:00:49 +0530

    Refactor AuthorNotFoundException to improve clarity by adding a default constructor with a specific error message.

--- src/main/java/com/westminster/bookstore/exceptions/AuthorNotFoundException.java
+++ src/main/java/com/westminster/bookstore/exceptions/AuthorNotFoundException.java
@@ -1,6 +1,12 @@
 package com.westminster.bookstore.exceptions;
 
 public class AuthorNotFoundException extends RuntimeException {
+    private static final long serialVersionUID = 1L;
+
+    public AuthorNotFoundException() {
+        super("author id does not exist");
+    }
+
     public AuthorNotFoundException(String message) {
         super(message);
     }
