ad6e157cec43dced9bb80abad5ee36b11beef09b
Thiwanka1012 <160256984+Thiwanka1012@users.noreply.github.com>
2025-04-27 16:48:27 +0530

    Refactor BookNotFoundExceptionMapper to simplify error response by removing unnecessary error map and returning a JSON string directly.

--- src/main/java/com/westminster/bookstore/exceptions/BookNotFoundExceptionMapper.java
+++ src/main/java/com/westminster/bookstore/exceptions/BookNotFoundExceptionMapper.java
@@ -3,16 +3,14 @@ package com.westminster.bookstore.exceptions;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.ext.ExceptionMapper;
 import javax.ws.rs.ext.Provider;
-import java.util.HashMap;
-import java.util.Map;
 
 @Provider
 public class BookNotFoundExceptionMapper implements ExceptionMapper<BookNotFoundException> {
     @Override
     public Response toResponse(BookNotFoundException exception) {
-        Map<String, String> error = new HashMap<>();
-        error.put("error", "Book Not Found");
-        error.put("message", exception.getMessage());
-        return Response.status(Response.Status.NOT_FOUND).entity(error).build();
+        return Response.status(Response.Status.NOT_FOUND)
+                .entity("{\"message\": \"book id does not exist\"}")
+                .type("application/json")
+                .build();
     }
 }
