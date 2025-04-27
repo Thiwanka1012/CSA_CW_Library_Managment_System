d271146d767e4c2fb4fdffc9313f9d8079240c69
Thiwanka1012 <160256984+Thiwanka1012@users.noreply.github.com>
2025-04-28 02:01:35 +0530

    Refactor AuthorDAO to enhance clarity by removing unnecessary comments and simplifying error handling in update and delete methods.

--- src/main/java/com/westminster/bookstore/dao/AuthorDAO.java
+++ src/main/java/com/westminster/bookstore/dao/AuthorDAO.java
@@ -8,7 +8,7 @@ import java.util.Map;
 
 public class AuthorDAO {
     private static Map<Integer, Author> authors = new HashMap<>();
-    private static int authorIdCounter = 0; // Counter for auto-generating authorId
+    private static int authorIdCounter = 0;
 
     public List<Author> getAllAuthors() {
         return new ArrayList<>(authors.values());
@@ -19,16 +19,13 @@ public class AuthorDAO {
     }
 
     public void addAuthor(Author author) {
-        // Auto-generate authorId if not provided
         if (author.getAuthorId() == 0) {
             authorIdCounter++;
             author.setAuthorId(authorIdCounter);
         } else {
-            // If authorId is provided, ensure it doesn't conflict
             if (authors.containsKey(author.getAuthorId())) {
                 throw new IllegalArgumentException("Author with ID " + author.getAuthorId() + " already exists.");
             }
-            // Update counter if the provided ID is greater
             if (author.getAuthorId() > authorIdCounter) {
                 authorIdCounter = author.getAuthorId();
             }
@@ -37,16 +34,10 @@ public class AuthorDAO {
     }
 
     public void updateAuthor(Author updatedAuthor) {
-        if (!authors.containsKey(updatedAuthor.getAuthorId())) {
-            throw new IllegalArgumentException("Author with ID " + updatedAuthor.getAuthorId() + " does not exist.");
-        }
         authors.put(updatedAuthor.getAuthorId(), updatedAuthor);
     }
 
     public void deleteAuthor(int id) {
-        if (!authors.containsKey(id)) {
-            throw new IllegalArgumentException("Author with ID " + id + " does not exist.");
-        }
         authors.remove(id);
     }
 }
